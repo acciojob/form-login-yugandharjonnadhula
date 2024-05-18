97ea48e79b4232980517caec1e3da4134637cf6a
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-05-18 21:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let firstname=document.getElementById("fname").value
+	let lastname=document.getElementById("lname").value
+	let fullname=firstname+" "+lastname;
+	alert(fullname);
 
 }
